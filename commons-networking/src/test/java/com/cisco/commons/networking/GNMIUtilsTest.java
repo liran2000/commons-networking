d1b2249887418ecaca12dd314203d9dc4eace330
lmendelo <lmendelo@cisco.com>
2021-01-10 08:40:05 +0200

    Adding copyrights

--- commons-networking/src/test/java/com/cisco/commons/networking/GNMIUtilsTest.java
+++ commons-networking/src/test/java/com/cisco/commons/networking/GNMIUtilsTest.java
@@ -7,6 +7,22 @@ import org.junit.Test;
 import gnmi.Gnmi.Path;
 import gnmi.Gnmi.PathElem;
 
+/**
+ * GNMI Path parser test.
+ * 
+ * @author Liran Mendelovich
+ * 
+ * Copyright 2019 Cisco Systems
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public class GNMIUtilsTest {
 	
 	@Test
